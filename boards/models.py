dfe89c471abb71190d8191d1b4a6fc8cc5ce946c
woonji913 <johnnyboy0913@gmail.com>
2019-03-14 06:12:32 +0000

    01_1:N model relation

--- boards/models.py
+++ boards/models.py
@@ -9,3 +9,28 @@ class Board(models.Model):
     
     def __str__(self):
         return f"{self.id}: {self.title}"
+        
+class Comment(models.Model):
+# 1. od_delete 
+#  참조하는 부모객체가 사라졌을 때, 부모에 딸려있는 자식 객체들을 어떻게 처리할지 정의한다.
+#  - 속성값
+#   1. CASCADE : 부모객체가 삭제됏을때 이를 참조하는 객체도 삭제한다.
+#   2. PROJECT : 참조가 되어있는 경우 삭제 오류 발생 (댓 달리면 삭제안됨)
+#   3. SET_NULL : 부모 객체가 삭제 됏을 때 참조하는 모든 값을 NULL로 치환 (NOT NULL 조건시 불가능)
+#   4. SET_DEFAULT : 모든 값이 DEFAULT로 치환. (디폴트 세팅이 필요)
+#   5. SET() : 특정 함수 호출.
+#   6. DO_NOTHING : 아무것도 하지 않음.(부모 게시글만 삭제되고 자식들만 남아있음) 
+#       - 다만 SQL에서는 on_delete 직접 설정해줘야함.
+# 2. 데이터베이스 무결성
+# 1) 개체 무결성: PK / NOT NULL / UNIQUE
+# -식별자는 NULL일수없고 중복일 수 없다.
+# 2) 참조 무결성: FK / 모든 외래 키의 값은 2가지 상태 가운데 하나에만 속함을 규정
+# 3) 범위/ 도메인무결성: 컬럼은 지정된 형식을 반드시 만족해야된다.(CHAR, TEXT)
+
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.content
--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect
-from .models import Board
+from .models import Board, Comment
 # from pprint import pprint
 
-# Create your views here.
+# # Create your views here.
+# def jdex(request):
+#     boards = Board.objects.order_by('-pk')
+#     context = {
+#         'boards' : boards,
+#     }
+#     return render(request, 'boards/jdex.html', context)
+    
 def index(request):
     # pprint(request)
     # pprint(type(request))
@@ -33,25 +40,29 @@ def new(request):
         # get 방식일 때
         return render(request, 'boards/new.html')
 
-def detail(request, pk):
-    board = Board.objects.get(pk=pk)
+def detail(request, board_pk):
+    board = Board.objects.get(pk=board_pk)
+    comments = board.comment_set.all()
+    
     context = {
         'board': board,
+        'comments': comments,
     }
     return render(request, 'boards/detail.html', context)
     
-def delete(request, pk):
-    board = Board.objects.get(pk=pk)
+def delete(request, board_pk):
+    board = Board.objects.get(pk=board_pk)
+    
     if request.method == 'POST':
         board.delete()
         return redirect('boards:index')
     else:
         return redirect('boards:detail', board.pk)
         
-def edit(request, pk):
+def edit(request,board_pk):
     if request.method == 'POST':
         # update
-        board = Board.objects.get(pk=pk)
+        board = Board.objects.get(pk=board_pk)
         board.title = request.POST.get('title')
         board.content = request.POST.get('content')
     
@@ -59,8 +70,29 @@ def edit(request, pk):
         return redirect('boards:detail', board.pk)
     else:
         # edit
-        board = Board.objects.get(pk=pk)
+        board = Board.objects.get(pk=board_pk)
         context = {
             'board': board,
             }
         return render(request, 'boards/edit.html', context)
+        
+def comments_create(request, board_pk):
+    # 댓글을 달 게시물
+    board = Board.objects.get(pk=board_pk)
+    
+    # form에서 넘어온 comment data
+    content = request.POST.get('content')
+    
+    # 댓글 생성 및 저장
+    comment = Comment(board=board, content=content)
+    comment.save()
+    return redirect('boards:detail', board.pk)
+    
+def comments_delete(request, board_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    if request.method == 'POST':
+        comment.delete()
+        return redirect('boards:detail', board_pk)
+    else:
+        return redirect('boards:detail', board_pk)
+    
